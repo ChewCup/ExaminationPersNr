38ecf0e794c32cf7f114ef101b9b3d84f4b6618f
ChewCup <72440370+ChewCup@users.noreply.github.com>
2020-10-27 09:04:44 +0100

    Add project files.

---
+++ ExaminationPersNr/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationPersNr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare
+            string userInput;
+            // user input
+            Console.Write("Enter your birth ID: ");
+            userInput = Console.ReadLine();
+            CheckInputs(userInput);
+            // convert input
+            int year = int.Parse(userInput.Substring(0, 4));
+
+            // stop
+            Console.ReadKey();
+        }
+        //check input length
+        static void CheckLenght(string input)
+        {
+            if (input.Length == 12)
+            {
+                Console.WriteLine("your lenght is " + input.Length);
+            }
+            else
+            {
+                Console.WriteLine("Enter 12 digit");
+            }
+        }
+        static void CheckInputs(string inputs)
+        {
+            CheckLenght(inputs);
+        }
+    }
+}
